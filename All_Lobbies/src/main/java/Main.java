831003b51a35ae1072fd37c5161201ecbdc79514
Emery Tom <zent.griffith@gmail.com>
2014-05-30 18:30:11 -0700

    Abc

---
+++ All_Lobbies/src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Emery
+ * Date: 5/30/14
+ * Time: 6:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
