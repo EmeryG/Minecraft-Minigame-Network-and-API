5928446f7f2d376cc7ff695add6dd8b817688c4a
Emery Tom <zent.griffith@gmail.com>
2014-06-01 19:56:05 -0700

    Revised the deck.

--- HubAPI/src/main/java/mineplicity/hubapi/main/database/Deck.java
+++ HubAPI/src/main/java/mineplicity/hubapi/main/database/Deck.java
@@ -1,10 +1,22 @@
 package mineplicity.hubapi.main.database;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Ervin on 6/1/2014.
  */
 public class Deck {
-    public enum cards {
+    @Getter @Setter
+    Card card1 = Card.FIREBALL;
+
+    @Getter @Setter
+    Card card2 = Card.FIREBALL;
+
+    @Getter @Setter
+    Card card3 = Card.FIREBALL;
+
+    public enum Card {
         X2,
         X4,
         X6,
