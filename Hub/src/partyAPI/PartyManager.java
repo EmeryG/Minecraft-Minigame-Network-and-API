31c7f31f81b1dcbc3e09d259333a25792029e80e
TheProfessionalAssassin <a.yasariz@gmail.com>
2014-06-04 15:16:56 -0700

    Added Erez's party code from BitBucket to GitHub.

---
+++ Hub/src/partyAPI/PartyManager.java
@@ -0,0 +1,34 @@
+package partyAPI;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
+public class PartyManager {
+
+	private static ArrayList<Party> partys = new ArrayList<Party>();
+	
+	public static void addParty(Player leader){
+		Party party = new Party(leader);
+		partys.add(party);
+	}
+	
+	public static void deleteParty(Party party){
+		partys.remove(party);
+		for(Player p : party.getPlayers()){
+			party.removePlayer(p);
+		}
+	}
+	
+	public static Party getParty(Player player){
+		for(Party p : partys){
+			if(p.getPlayers().contains(player)) return p;
+		}
+		return null;
+	}
+	
+	public static void disbandParty(Party party){
+		deleteParty(party);
+	}
+	
+}
