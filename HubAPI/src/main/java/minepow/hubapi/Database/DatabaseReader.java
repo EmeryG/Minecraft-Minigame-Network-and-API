6d8b1bba4d36b2ba844ca48aae6692105f8930ed
Emery Tom <zent.griffith@gmail.com>
2014-06-06 21:56:38 -0700

    Added database and economy packages and classes.

---
+++ HubAPI/src/main/java/minepow/hubapi/Database/DatabaseReader.java
@@ -0,0 +1,11 @@
+package minepow.hubapi.Database;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Emery
+ * Date: 6/6/14
+ * Time: 9:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DatabaseReader {
+}
