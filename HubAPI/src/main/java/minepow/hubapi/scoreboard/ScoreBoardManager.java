cc34af980240a932e760c7c3febcab92587b6567
erez9901 <erez9901@gmail.com>
2014-06-07 19:17:18 +0300

    Scoreboard

--- HubAPI/src/main/java/minepow/hubapi/scoreboard/ScoreBoardManager.java
+++ HubAPI/src/main/java/minepow/hubapi/scoreboard/ScoreBoardManager.java
@@ -1,30 +1,33 @@
 package minepow.hubapi.scoreboard;
 
-import minepow.hubapi.Main;
 import minepow.hubapi.economy.EconomyManager;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.*;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
 /**
  * Created by Ervin on 6/7/2014.
  */
 public class ScoreBoardManager {
-
-    public static void scoreboard(Player p) {
-        ScoreboardManager manager = Bukkit.getScoreboardManager();
-        Scoreboard board = manager.getNewScoreboard();
-
-        Objective objective = board.registerNewObjective("Test", "Test2");
-        objective.setDisplayName(Main.plugin.getConfig().getString("SCOREBOARD_TITLE").replaceAll("§","&"));
-        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-
-        Score score = objective.getScore(Bukkit.getOfflinePlayer(Main.plugin.getConfig().getString("SOREBOARD_MONEY").replaceAll("§","&")));
-        score.setScore(EconomyManager.getCurrentMoney(p));
-
-        p.setScoreboard(board);
-    }
+	
+	static Scoreboard scoreboard = null;
+	static Objective title = null;
+	private static final String TITLE = ChatColor.GOLD + "MinePow";
+	
+	@SuppressWarnings("deprecation")
+	public static void updateScoreBoard(Player player){
+		
+		//creating a new score board
+		scoreboard  = Bukkit.getServer().getScoreboardManager().getNewScoreboard();
+		//creating an objective called ^TITLE^ 
+		title = scoreboard.registerNewObjective(TITLE, "dummy");
+		
+		//Setting some scores
+		title.getScore(Bukkit.getServer().getOfflinePlayer(ChatColor.GOLD + "" + ChatColor.BOLD + "PowMoney:")).setScore(15);;
+		title.getScore(Bukkit.getServer().getOfflinePlayer(ChatColor.AQUA + "" + ChatColor.BOLD + EconomyManager.getCurrentMoney(player))).setScore(14);;
+	}
 	
 }
