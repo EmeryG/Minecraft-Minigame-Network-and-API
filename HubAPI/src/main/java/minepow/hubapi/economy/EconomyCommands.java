0f38a44a3bbb3a05239be26860ca91527c2d0508
Torn <tiarlninbox@gmail.com>
2014-06-07 09:48:32 -0600

    dzfxsfgf

--- HubAPI/src/main/java/minepow/hubapi/economy/EconomyCommands.java
+++ HubAPI/src/main/java/minepow/hubapi/economy/EconomyCommands.java
@@ -7,42 +7,41 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class EconomyCommands implements CommandExecutor{
+public class EconomyCommands implements CommandExecutor {
 
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		if (!(sender instanceof Player)) {
-			sender.sendMessage(ChatColor.RED + "Only player can do this command!");
-			return true;
-		}
-		Player p = (Player) sender;
-		
-		if(args.length == 0){
-			p.sendMessage(ChatColor.RED + "/money balance");
-			p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
-			return true;
-		}
-		
-		if(args[0].equalsIgnoreCase("balance")){
-			p.sendMessage(ChatColor.RED + "Current PowMoney: " + ChatColor.RESET + EconomyManager.getCurrentMoney(p));
-			return true;
-		}
-		if(args[0].equalsIgnoreCase("pay")){
-			if(args.length < 2){
-				p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
-				return true;	
-			}
-			
-			@SuppressWarnings("deprecation")
-			Player t = Bukkit.getServer().getPlayer(args[12]);
-			if(t == null){
-				p.sendMessage(ChatColor.RED + "Can't find player!");
-				return true;
-			}
-			EconomyManager.payForPlayer(p, t, Integer.parseInt(args[0]));
-			return true;
-		}
-		
-		return true;
-	}
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only player can do this command!");
+            return true;
+        }
+        Player p = (Player) sender;
+        if (cmd.getName().equalsIgnoreCase("money")) {
+            if (args.length == 0) {
+                p.sendMessage(ChatColor.RED + "/money balance");
+                p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
+                return true;
+            }
 
+            if (args[0].equalsIgnoreCase("balance")) {
+                p.sendMessage(ChatColor.RED + "Current PowMoney: " + ChatColor.RESET + EconomyManager.getCurrentMoney(p));
+                return true;
+            }
+            if (args[0].equalsIgnoreCase("pay")) {
+                if (args.length < 2) {
+                    p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
+                    return true;
+                }
+
+                @SuppressWarnings("deprecation")
+                Player t = Bukkit.getServer().getPlayer(args[12]);
+                if (t == null) {
+                    p.sendMessage(ChatColor.RED + "Can't find player!");
+                    return true;
+                }
+                EconomyManager.payForPlayer(p, t, Integer.parseInt(args[0]));
+                return true;
+            }
+        }
+        return true;
+    }
 }
