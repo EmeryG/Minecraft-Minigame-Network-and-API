37acb7ec95acc711ba72ef8e70ab7f0eb70cec33
erez9901 <erez9901@gmail.com>
2014-06-07 17:40:36 +0300

    New args for commands

--- HubAPI/src/main/java/minepow/hubapi/economy/EconomyCommands.java
+++ HubAPI/src/main/java/minepow/hubapi/economy/EconomyCommands.java
@@ -1,5 +1,6 @@
 package minepow.hubapi.economy;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,7 +16,31 @@ public class EconomyCommands implements CommandExecutor{
 		}
 		Player p = (Player) sender;
 		
-		p.sendMessage(ChatColor.RED + "Current PowMoney: " + ChatColor.RESET + EconomyManager.getCurrentMoney(p));
+		if(args.length == 0){
+			p.sendMessage(ChatColor.RED + "/money balance");
+			p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
+			return true;
+		}
+		
+		if(args[0].equalsIgnoreCase("balance")){
+			p.sendMessage(ChatColor.RED + "Current PowMoney: " + ChatColor.RESET + EconomyManager.getCurrentMoney(p));
+			return true;
+		}
+		if(args[0].equalsIgnoreCase("pay")){
+			if(args.length < 2){
+				p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
+				return true;	
+			}
+			
+			@SuppressWarnings("deprecation")
+			Player t = Bukkit.getServer().getPlayer(args[12]);
+			if(t == null){
+				p.sendMessage(ChatColor.RED + "Can't find player!");
+				return true;
+			}
+			EconomyManager.payForPlayer(p, t, Integer.parseInt(args[0]));
+			return true;
+		}
 		
 		return true;
 	}
