408e27a12072510359fad1aabf1ec27bc50c7e7a
Torn <tiarlninbox@gmail.com>
2014-06-07 00:09:41 -0600

    Changes to partyCreate

--- HubAPI/src/main/java/minepow/hubapi/partyapi/PartyReciever.java
+++ HubAPI/src/main/java/minepow/hubapi/partyapi/PartyReciever.java
@@ -13,44 +13,44 @@ import org.bukkit.entity.Player;
  */
 public class PartyReciever {
 
-    public static void RecieveMessage(String message){
-        if(message.startsWith("d=")) {
+    public static void RecieveMessage(String message) {
+        if (message.startsWith("d=")) {
             message = message.substring(2);
             partyDisband(Bukkit.getOfflinePlayer(message));
 
-        } else if(message.startsWith("c=")) {
+        } else if (message.startsWith("c=")) {
             message = message.substring(2);
             partyCreate(Bukkit.getOfflinePlayer(message));
 
-        } else if(message.startsWith("i=")) {
+        } else if (message.startsWith("i=")) {
             message = message.substring(2);
             String leader = "";
-            for(int i = 0; i < message.length(); i++) {
-                if(message.charAt(i) == ';') {
-                    leader = message.substring(i+1);
+            for (int i = 0; i < message.length(); i++) {
+                if (message.charAt(i) == ';') {
+                    leader = message.substring(i + 1);
                 }
             }
             partyInvite(Bukkit.getOfflinePlayer(message), Bukkit.getOfflinePlayer(leader));
 
-        } else if(message.startsWith("a=")) {
+        } else if (message.startsWith("a=")) {
             message = message.substring(2);
             partyAccept(Bukkit.getOfflinePlayer(message));
 
-        } else if(message.startsWith("l=")) {
+        } else if (message.startsWith("l=")) {
             message = message.substring(2);
             partyLeave(Bukkit.getOfflinePlayer(message));
 
-        } else if(message.startsWith("chat=")) {
+        } else if (message.startsWith("chat=")) {
             message = message.substring(5);
             String chatMessage = "";
-            for(int i = 0; i < message.length(); i++) {
-                if(message.charAt(i) == ';') {
-                    chatMessage = message.substring(i+1);
+            for (int i = 0; i < message.length(); i++) {
+                if (message.charAt(i) == ';') {
+                    chatMessage = message.substring(i + 1);
                 }
             }
             partyChat(Bukkit.getOfflinePlayer(message), chatMessage);
 
-        } else if(message.startsWith("k=")) {
+        } else if (message.startsWith("k=")) {
             message = message.substring(2);
             partyKick(Bukkit.getOfflinePlayer(message));
         }
@@ -62,21 +62,20 @@ public class PartyReciever {
     }
 
     static void partyCreate(OfflinePlayer leader) {
-        Party party = new Party(leader);
+        PartyManager.addParty(leader);
     }
 
     static void partyInvite(OfflinePlayer p, OfflinePlayer leader) {
-        if(PartyManager.invites.containsKey(p)){
+        if (PartyManager.invites.containsKey(p)) {
             PartyManager.invites.remove(p);
             PartyManager.invites.put(p, leader);
-        }
-        else{
+        } else {
             PartyManager.invites.put(p, leader);
         }
     }
 
     static void partyAccept(OfflinePlayer p) {
-        if(PartyManager.invites.containsKey(p)){
+        if (PartyManager.invites.containsKey(p)) {
             Party party = PartyManager.getParty(PartyManager.invites.get(p));
             party.addMember(p);
             PartyManager.invites.remove(p);
@@ -85,15 +84,15 @@ public class PartyReciever {
 
     static void partyLeave(OfflinePlayer p) {
         Party party = PartyManager.getParty(p);
-        if(party != null){
+        if (party != null) {
             party.deleteMember(p);
         }
     }
 
     static void partyChat(OfflinePlayer p, String text) {
         Party party = PartyManager.getParty(p);
-        for(OfflinePlayer player : party.getMembers()){
-            if(player.isOnline()) {
+        for (OfflinePlayer player : party.getMembers()) {
+            if (player.isOnline()) {
                 Player oPlayer = (Player) player;
                 oPlayer.sendMessage(text);
             }
