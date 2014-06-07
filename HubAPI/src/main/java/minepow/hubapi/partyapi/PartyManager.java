8efceb4fc4d3b1213cfc8c74bb4ae5f30694b279
Emery Tom <zent.griffith@gmail.com>
2014-06-06 18:44:37 -0700

    Basic party classes.

---
+++ HubAPI/src/main/java/minepow/hubapi/partyapi/PartyManager.java
@@ -0,0 +1,11 @@
+package minepow.hubapi.partyapi;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Emery
+ * Date: 6/6/14
+ * Time: 6:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PartyManager {
+}
