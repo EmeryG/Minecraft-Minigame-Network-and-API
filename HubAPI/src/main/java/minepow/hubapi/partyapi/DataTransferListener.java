9e7ae872d5be186d28f328b4d652b56c4e093bdb
Torn <tiarlninbox@gmail.com>
2014-06-06 22:14:30 -0600

    Space changes

--- HubAPI/src/main/java/minepow/hubapi/partyapi/DataTransferListener.java
+++ HubAPI/src/main/java/minepow/hubapi/partyapi/DataTransferListener.java
@@ -7,31 +7,28 @@ import java.net.Socket;
 /**
  * Created by Connor Peticca on 6/6/2014.
  */
-public class DataTransferListener extends  Thread{
+public class DataTransferListener extends Thread {
 
     @Override
     public void run() {
 
-        while (true){
+        while (true) {
 
-            try{
+            try {
 
-                for (Socket Sock : ServerConnections.Connections){
+                for (Socket Sock : ServerConnections.Connections) {
 
                     BufferedReader br = new BufferedReader(new InputStreamReader(Sock.getInputStream()));
                     String message;
 
-                    if((message = br.readLine()) != null){
+                    if ((message = br.readLine()) != null) {
 
                         PartyReciever.RecieveMessage(message);
 
                     }
-
                 }
-
-            }catch (Exception e){}
-
+            } catch (Exception e) {
+            }
         }
-
     }
 }
--- HubAPI/src/main/java/minepow/hubapi/partyapi/ServerConnections.java
+++ HubAPI/src/main/java/minepow/hubapi/partyapi/ServerConnections.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 /**
  * Created by Connor Peticca on 6/6/2014.
  */
-public class ServerConnections extends Thread{
+public class ServerConnections extends Thread {
 
     public static ServerSocket ServerSock;
     public static ArrayList<Socket> Connections = new ArrayList<Socket>();
@@ -17,28 +17,26 @@ public class ServerConnections extends Thread{
     @Override
     public void run() {
 
-
-
-        try{
+        try {
 
             ServerSock = new ServerSocket(Main.plugin.getConfig().getInt("Port"));
             Socket Sock;
 
-            while (true){
+            while (true) {
 
                 do {
 
                     Sock = ServerSock.accept();
-                    if(!Connections.contains(Sock)){Connections.add(Sock);}
+                    if (!Connections.contains(Sock)) {
+                        Connections.add(Sock);
+                    }
 
-                }while (Sock == null);
-
+                } while (Sock == null);
 
 
             }
 
-        }catch (Exception e){}
-
-
+        } catch (Exception e) {
+        }
     }
 }
