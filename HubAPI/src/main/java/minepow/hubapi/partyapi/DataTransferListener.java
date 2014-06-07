374907400fef780233be519f7a67657aef552dec
Cuhlifa <bosscrypt@hotmail.com>
2014-06-06 23:10:52 -0400

    sdafv,asb,dnfbkb,,f

---
+++ HubAPI/src/main/java/minepow/hubapi/partyapi/DataTransferListener.java
@@ -0,0 +1,37 @@
+package minepow.hubapi.partyapi;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * Created by Connor Peticca on 6/6/2014.
+ */
+public class DataTransferListener extends  Thread{
+
+    @Override
+    public void run() {
+
+        while (true){
+
+            try{
+
+                for (Socket Sock : ServerConnections.Connections){
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(Sock.getInputStream()));
+                    String message;
+
+                    if((message = br.readLine()) != null){
+
+                        PartyReciever.RecieveMessage(message);
+
+                    }
+
+                }
+
+            }catch (Exception e){}
+
+        }
+
+    }
+}
