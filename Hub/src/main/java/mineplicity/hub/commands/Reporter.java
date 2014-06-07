3f1b4fd6298e0ce8aa0bc7a69802540f1f6cfba7
Spencer Dixon <spencerdixon16@gmail.com>
2014-06-07 01:13:56 -0400

    Updates to Reporter Command.

--- Hub/src/main/java/mineplicity/hub/commands/Reporter.java
+++ Hub/src/main/java/mineplicity/hub/commands/Reporter.java
@@ -6,6 +6,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Ali on 01/06/2014.
  */
@@ -18,15 +22,16 @@ public class Reporter implements CommandExecutor {
             Player player = (Player) commandSender;
 
             String message = "";
-
+            HashMap<String, String> reports = new HashMap<String, String>();
             for(int i = 1; i< strings.length; i++){
                 message += strings[i];
+                reports.put(player.getName(), message);
             }
 
-            Main.config.set("Report.Messages", player.getName() + ", " + message);
+            Main.config.set("Report.Messages", reports);
 
         }else if(strings[0].equalsIgnoreCase("list")){
-
+            Main.config.getList("Reports.Messages");
         }
 
 
