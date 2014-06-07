290a97393a5855ea206a9c9817d7424e0085decd
Torn <tiarlninbox@gmail.com>
2014-06-07 11:55:27 -0600

    Switched from If to Case statements.

--- HubAPI/src/main/java/minepow/hubapi/partyapi/PartyCommands.java
+++ HubAPI/src/main/java/minepow/hubapi/partyapi/PartyCommands.java
@@ -30,63 +30,59 @@ public class PartyCommands implements CommandExecutor {
 
             Player player = (Player) sender;
 
-            if (args[0].equalsIgnoreCase("create")) {
-                PartySender.partyCreate(player);
-                player.sendMessage(Main.plugin.getConfig().getString("PARTY_CREATE").replaceAll("§","&"));
-                return true;
-
-            } else if (args[0].equalsIgnoreCase("disband")) {
-                if(PartyManager.getParty(player) != null) {
-                    PartySender.partyDisband(player);
-                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_DISBAND").replaceAll("§","&"));
-                    return true;
-                } else {
-                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_DISBAND_ERROR").replaceAll("§","&"));
-                    return false;
-                }
-
-            } else if (args[0].equalsIgnoreCase("invite")) {
-                Player target = Bukkit.getPlayer(args[1]);
-                OfflinePlayer party = PartyManager.getParty(player).getLeader();
-                if(party != null) {
-                    PartySender.partyInvite(target, party);
-                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_INVITE").replaceAll("§","&"));
-                    return true;
-                }
-
-            } else if (args[0].equalsIgnoreCase("accept")) {
-                Party currentParty = PartyManager.getParty(player);
-                if(currentParty == null) {
-                    PartySender.partyAccept(player);
-                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_JOIN").replaceAll("§","&"));
-                    return true;
-                }
-
-            } else if (args[0].equalsIgnoreCase("leave")) {
-                Party party = PartyManager.getParty(player);
-                if(party != null) {
-                    PartySender.partyLeave(player);
-                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_LEAVE").replaceAll("§","&"));
-                    return true;
-                }
-
-            } else if (args[0].equalsIgnoreCase("chat")) {
-                OfflinePlayer leader = PartyManager.getParty(player).getLeader();
-                String message = "";
-                for(int i = 1; i < args.length; i++) {
-                    message += args[i];
-                }
-                PartySender.partyChat(player, message);
-                player.sendMessage(Main.plugin.getConfig().getString("PARTY_CHAT").replaceAll("§","&"));
-                return true;
-
-            } else if(args[0].equalsIgnoreCase("kick")){
-                PartySender.partyKick(player);
-                player.sendMessage(Main.plugin.getConfig().getString("PARTY_KICK").replaceAll("§","&"));
-            } else {
-                return true;
+            switch (args[0].toLowerCase()) {
+                case "create":
+                    PartySender.partyCreate(player);
+                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_CREATE").replaceAll("§", "&"));
+                    break;
+                case "disband":
+                    if (PartyManager.getParty(player) != null) {
+                        PartySender.partyDisband(player);
+                        player.sendMessage(Main.plugin.getConfig().getString("PARTY_DISBAND").replaceAll("§", "&"));
+                        return true;
+                    } else {
+                        player.sendMessage(Main.plugin.getConfig().getString("PARTY_DISBAND_ERROR").replaceAll("§", "&"));
+                    }
+                    break;
+                case "invite":
+                    Player target = Bukkit.getPlayer(args[1]);
+                    OfflinePlayer party = PartyManager.getParty(player).getLeader();
+                    if (party != null) {
+                        PartySender.partyInvite(target, party);
+                        player.sendMessage(Main.plugin.getConfig().getString("PARTY_INVITE").replaceAll("§", "&"));
+                        return true;
+                    }
+                    break;
+                case "accept":
+                    Party currentParty = PartyManager.getParty(player);
+                    if (currentParty == null) {
+                        PartySender.partyAccept(player);
+                        player.sendMessage(Main.plugin.getConfig().getString("PARTY_JOIN").replaceAll("§", "&"));
+                        return true;
+                    }
+                    break;
+                case "leave":
+                    Party partyleave = PartyManager.getParty(player);
+                    if (partyleave != null) {
+                        PartySender.partyLeave(player);
+                        player.sendMessage(Main.plugin.getConfig().getString("PARTY_LEAVE").replaceAll("§", "&"));
+                        return true;
+                    }
+                    break;
+                case "chat":
+                    OfflinePlayer leader = PartyManager.getParty(player).getLeader();
+                    String message = "";
+                    for (int i = 1; i < args.length; i++) {
+                        message += args[i];
+                    }
+                    PartySender.partyChat(player, message);
+                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_CHAT").replaceAll("§", "&"));
+                    break;
+                case "kick":
+                    PartySender.partyKick(player);
+                    player.sendMessage(Main.plugin.getConfig().getString("PARTY_KICK").replaceAll("§", "&"));
             }
         }
-        return false;
+        return true;
     }
 }
