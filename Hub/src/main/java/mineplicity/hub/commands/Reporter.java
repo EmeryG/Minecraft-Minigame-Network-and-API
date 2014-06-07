4c72a1cb1198d83c415d668f70f25e9fcaab0cd1
Spencer Dixon <spencerdixon16@gmail.com>
2014-06-07 01:05:19 -0400

    Updates to Reporter Command in Hub

--- Hub/src/main/java/mineplicity/hub/commands/Reporter.java
+++ Hub/src/main/java/mineplicity/hub/commands/Reporter.java
@@ -1,8 +1,10 @@
 package mineplicity.hub.commands;
 
+import mineplicity.hub.main.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  * Created by Ali on 01/06/2014.
@@ -10,6 +12,24 @@ import org.bukkit.command.CommandSender;
 public class Reporter implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        if(s.equals("report")){
+
+            Player player = (Player) commandSender;
+
+            String message = "";
+
+            for(int i = 1; i< strings.length; i++){
+                message += strings[i];
+            }
+
+            Main.config.set("Report.Messages", player.getName() + ", " + message);
+
+        }else if(strings[0].equalsIgnoreCase("list")){
+
+        }
+
+
         return false;
     }
 }
