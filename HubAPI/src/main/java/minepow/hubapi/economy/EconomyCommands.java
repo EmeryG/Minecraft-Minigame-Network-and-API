0fe381acb1719470dcdfb4a99b5654d314b4207c
Torn <tiarlninbox@gmail.com>
2014-06-07 13:43:39 -0600

    Scoreboardqweeeeeeeeeeeeeeeeeeee

--- HubAPI/src/main/java/minepow/hubapi/economy/EconomyCommands.java
+++ HubAPI/src/main/java/minepow/hubapi/economy/EconomyCommands.java
@@ -15,32 +15,29 @@ public class EconomyCommands implements CommandExecutor {
             return true;
         }
         Player p = (Player) sender;
-        
-            if (args.length == 0) {
-                p.sendMessage(ChatColor.RED + "/money balance");
+
+        if (args.length == 0) {
+            p.sendMessage(ChatColor.RED + "/money balance");
+            p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
+            return true;
+        } else if (args[0].equalsIgnoreCase("balance")) {
+            p.sendMessage(ChatColor.RED + "Current PowMoney: " + ChatColor.RESET + EconomyManager.getCurrentMoney(p));
+            return true;
+        } else if (args[0].equalsIgnoreCase("pay")) {
+            if (args.length < 2) {
                 p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
                 return true;
             }
 
-            if (args[0].equalsIgnoreCase("balance")) {
-                p.sendMessage(ChatColor.RED + "Current PowMoney: " + ChatColor.RESET + EconomyManager.getCurrentMoney(p));
+            @SuppressWarnings("deprecation")
+            Player t = Bukkit.getServer().getPlayer(args[12]);
+            if (t == null) {
+                p.sendMessage(ChatColor.RED + "Can't find player!");
                 return true;
             }
-            if (args[0].equalsIgnoreCase("pay")) {
-                if (args.length < 2) {
-                    p.sendMessage(ChatColor.RED + "/money pay <Amount> <Player>");
-                    return true;
-                }
-
-                @SuppressWarnings("deprecation")
-                Player t = Bukkit.getServer().getPlayer(args[12]);
-                if (t == null) {
-                    p.sendMessage(ChatColor.RED + "Can't find player!");
-                    return true;
-                }
-                EconomyManager.payForPlayer(p, t, Integer.parseInt(args[0]));
-                return true;
-            }
+            EconomyManager.payForPlayer(p, t, Integer.parseInt(args[0]));
+            return true;
+        }
         return true;
     }
 }
