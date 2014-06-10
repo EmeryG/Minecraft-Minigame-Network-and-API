e57a2927429c4f9852c4dbbddc7b2df50b755d46
erez9901 <erez9901@gmail.com>
2014-06-10 21:36:50 +0300

    News Listener

--- Hub/src/main/java/mineplicity/hub/news/NewsListener.java
+++ Hub/src/main/java/mineplicity/hub/news/NewsListener.java
@@ -11,54 +11,65 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class NewsListener implements Listener {
 
-    Main plugin;
-    ArrayList<Player> players = new ArrayList<Player>();
+	Main plugin;
+	ArrayList<Player> players = new ArrayList<Player>();
 
-    public NewsListener(Main plugin) {
-        this.plugin = plugin;
-        Main.config = plugin.getConfig();
-    }
+	public NewsListener(Main plugin) {
+		this.plugin = plugin;
+		Main.config = plugin.getConfig();
+	}
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        Player p = e.getPlayer();
-        start(p);
-    }
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		if (!players.contains(p)) {
+			start(p);
+		}
+	}
 
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent e) {
-        Player p = e.getPlayer();
-        if (!players.contains(p)) {
-            start(p);
-        }
-    }
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent e) {
+		Player p = e.getPlayer();
+		players.remove(p);
+	}
 
-    public void start(final Player player) {
-        players.add(player);
-        int x = 1;
-        final int[] num = {x};
-        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-            public void run() {
-                if (Main.config.getString("News." + num[0]) == null) {
-                    num[0] = 1;
-                    return;
-                }
-                BarAPI.setMessage(player, Main.config.getString("News." + num[0]).replace("&", "�"), (float) num[0] / getMaxMsg() * 100);
-                num[0]++;
-            }
-        }, 20 * 4, 20 * 2);
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent e) {
+		Player p = e.getPlayer();
+		if (!players.contains(p)) {
+			start(p);
+		}
+	}
 
-    }
+	public void start(final Player player) {
+		if (players.contains(player))
+			return;
+		players.add(player);
+		int x = 1;
+		final int[] num = { x };
+		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+			public void run() {
+				if (Main.config.getString("News." + num[0]) == null) {
+					num[0] = 1;
+					return;
+				}
+				BarAPI.setMessage(player, Main.config.getString("News." + num[0]).replace("&", "�"), (float) num[0] / getMaxMsg() * 100);
+				num[0]++;
+			}
+		}, 20 * 4, 20 * 2);
 
-    public int getMaxMsg() {
-        for (int x = 1; x < 10; x++) {
-            if (Main.config.getString("News." + x) == null) {
-                return x - 1;
-            }
-        }
-        return 0;
-    }
+	}
+
+	public int getMaxMsg() {
+		for (int x = 1; x < 10; x++) {
+			if (Main.config.getString("News." + x) == null) {
+				return x - 1;
+			}
+		}
+		return 0;
+	}
 }
