26c0094697a6bc6e19c84e3078f4480db881ebe0
EpicSauceBro <a.yasariz@gmail.com>
2014-06-15 11:04:07 -0700

    Stuff

--- Minigame_Instance/src/java/listeners/PlayerDeath.java
+++ Minigame_Instance/src/java/listeners/PlayerDeath.java
@@ -2,6 +2,9 @@ package listeners;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import player.PlayerManager;
+import spectator.Spectator;
+import spectator.SpectatorManager;
 
 /**
  * Created by Ali on 14/06/2014.
@@ -10,6 +13,8 @@ public class PlayerDeath implements Listener{
 
     public void onPDE(PlayerDeathEvent e){
         //Do spectator stuff
+        PlayerManager.removePlayer(e.getEntity());
+        SpectatorManager.removeSpectator(e.getEntity());
     }
 
 }
