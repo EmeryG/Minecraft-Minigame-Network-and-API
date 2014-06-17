2c29958437650b27aed00a1a1da0f294f5b6d0d7
EpicSauceBro <a.yasariz@gmail.com>
2014-06-16 19:20:07 -0700

    Conflict fix

--- Minigame_Instance/src/java/minepow/Main.java
+++ Minigame_Instance/src/java/minepow/Main.java
@@ -1,9 +1,5 @@
-<<<<<<< HEAD:Minigame_Instance/src/java/Main.java
-import LobbyStage.Timer;
-=======
 package minepow;
 
->>>>>>> 14d030dfc2bdb84fad71cf859ad0b500646ce46e:Minigame_Instance/src/java/minepow/Main.java
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
@@ -18,11 +14,7 @@ public class Main extends JavaPlugin{
 
     @Override
     public void onEnable(){
-<<<<<<< HEAD:Minigame_Instance/src/java/Main.java
-        
-=======
         main = this;
->>>>>>> 14d030dfc2bdb84fad71cf859ad0b500646ce46e:Minigame_Instance/src/java/minepow/Main.java
     }
 
     @Override
