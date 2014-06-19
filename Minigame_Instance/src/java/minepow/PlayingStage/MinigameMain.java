71f5ffdf855887bf70bb0cc2117a417f3416ec73
Emery Tom <zent.griffith@gmail.com>
2014-06-18 18:52:31 -0700

    Added spectators

--- Minigame_Instance/src/java/minepow/PlayingStage/MinigameMain.java
+++ Minigame_Instance/src/java/minepow/PlayingStage/MinigameMain.java
@@ -1,5 +1,14 @@
 package minepow.PlayingStage;
 
+import minepow.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Emery
@@ -8,5 +17,31 @@ package minepow.PlayingStage;
  * To change this template use File | Settings | File Templates.
  */
 public class MinigameMain {
+    static ArrayList<Player> spectators = new ArrayList<Player>();
+    static ArrayList<Listener> listeners = new ArrayList<Listener>();
+    static ArrayList<BukkitRunnable> threads = new ArrayList<BukkitRunnable>();
 
+    public static void registerListener(Listener listener) {
+        listeners.add(listener);
+        Bukkit.getPluginManager().registerEvents(listener, Main.getMain());
+    }
+
+    public static void registerThread(BukkitRunnable thread) {
+        threads.add(thread);
+    }
+
+    public static void setSpecator(Player spectator) {
+        spectators.add(spectator);
+    }
+
+    public static void finish() {
+        for(Listener l : listeners) {
+            HandlerList.unregisterAll(l);
+        }
+        listeners.clear();
+        for(BukkitRunnable thread : threads) {
+            thread.cancel();
+        }
+        threads.clear();
+    }
 }
