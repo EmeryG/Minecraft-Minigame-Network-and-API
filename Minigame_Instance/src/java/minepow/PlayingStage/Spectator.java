0019604398ed4ec298cf7ba454cddae5110f6df1
Torn <tiarlninbox@gmail.com>
2014-06-19 16:06:31 -0500

    spectator

--- Minigame_Instance/src/java/minepow/PlayingStage/Spectator.java
+++ Minigame_Instance/src/java/minepow/PlayingStage/Spectator.java
@@ -3,10 +3,13 @@ package minepow.PlayingStage;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 
 /**
@@ -16,7 +19,7 @@ public class Spectator implements Listener {
 
     @EventHandler
     public void onPDE(EntityDamageEvent event) {
-        if (event instanceof Player) {
+        if (event.getEntity() instanceof Player) {
             Player player = ((Player) event).getPlayer();
             if (MinigameMain.getSpecators().contains(player)) {
                 event.setCancelled(true);
@@ -26,7 +29,7 @@ public class Spectator implements Listener {
 
     @EventHandler
     public void onPDIP(PlayerDropItemEvent event) {
-        if (event instanceof Player) {
+        if (event.getPlayer() instanceof Player) {
             Player player = event.getPlayer();
             if (MinigameMain.getSpecators().contains(player)) {
                 event.setCancelled(true);
@@ -36,7 +39,7 @@ public class Spectator implements Listener {
 
     @EventHandler
     public void onPHE(FoodLevelChangeEvent event) {
-        if (event instanceof Player) {
+        if (event.getEntity() instanceof Player) {
             Player player = ((Player) event).getPlayer();
             if (MinigameMain.getSpecators().contains(player)) {
                 event.setCancelled(true);
@@ -46,7 +49,7 @@ public class Spectator implements Listener {
 
     @EventHandler
     public void onPPIE(PlayerPickupItemEvent event) {
-        if (event instanceof Player) {
+        if (event.getPlayer() instanceof Player) {
             Player player = event.getPlayer();
             if (MinigameMain.getSpecators().contains(player)) {
                 event.setCancelled(true);
@@ -56,9 +59,41 @@ public class Spectator implements Listener {
 
     @EventHandler
     public void onPDE(EntityDamageByEntityEvent event) {
-        if (event instanceof Player) {
-            Player player = ((Player) event).getPlayer();
+        if (event.getDamager() instanceof Player) {
             Player damager = (Player) event.getDamager();
+            if (MinigameMain.getSpecators().contains(damager)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPIE(PlayerInteractEvent event) {
+        if (event.getPlayer() instanceof Player) {
+            Player player = event.getPlayer();
+            if (MinigameMain.getSpecators().contains(player)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPBE(BlockBreakEvent event) {
+        if (event.getPlayer() instanceof Player) {
+            Player player = event.getPlayer();
+            if (MinigameMain.getSpecators().contains(player)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPPE(BlockPlaceEvent event) {
+        if (event.getPlayer() instanceof Player) {
+            Player player = event.getPlayer();
+            if (MinigameMain.getSpecators().contains(player)) {
+                event.setCancelled(true);
+            }
         }
     }
 }
