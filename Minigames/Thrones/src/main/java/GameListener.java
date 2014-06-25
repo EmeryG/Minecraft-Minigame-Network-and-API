493bf8725d67036e242811bc114fa74c1d5088f3
EpicSauceBro <a.yasariz@gmail.com>
2014-06-25 09:09:43 -0700

    Finishing up thrones. Kits are left still

--- Minigames/Thrones/src/main/java/GameListener.java
+++ Minigames/Thrones/src/main/java/GameListener.java
@@ -3,7 +3,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ali on 24/06/2014.
@@ -13,7 +14,7 @@ public class GameListener implements Listener{
     public void onEDBEE(EntityDamageByEntityEvent e){
         if(e.getEntity() instanceof Player) {
             if (((Player) e.getEntity()).getHealth() - e.getDamage() < 1) {
-                if (States)
+                if (StatesListener.bodyguards.size() > 0 && StatesListener.rebels.size() > 0) {
                     if (StatesListener.bodyguards.contains(e.getEntity())) {
                         StatesListener.bodyguards.remove(e.getEntity());
                         StatesListener.rebels.add((org.bukkit.entity.Player) e.getEntity());
@@ -21,9 +22,22 @@ public class GameListener implements Listener{
                         StatesListener.rebels.remove(e.getEntity());
                         StatesListener.silverfish.add((org.bukkit.entity.Player) e.getEntity());
                     }
+                }else{
+                    if(StatesListener.bodyguards.size() < 1){
+                        ArrayList<Player> players = StatesListener.rebels;
+                        players.addAll(StatesListener.silverfish);
+                        MinigameMain.finish(players);
+                    }else if(StatesListener.rebels.size() < 1){
+                        MinigameMain.finish(StatesListener.bodyguards);
+                    }
+                }
             }
         }else{
-            MinigameMain.finish(StatesListener.rebels);
+            if(e.getEntity().isDead()) {
+                ArrayList<Player> players = StatesListener.rebels;
+                players.addAll(StatesListener.silverfish);
+                MinigameMain.finish(players);
+            }
         }
     }
 }
